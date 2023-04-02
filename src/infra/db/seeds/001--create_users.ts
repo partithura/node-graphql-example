51b2dc52fa436407776fb0c872d311df74346793
Vinicius Reis <luiz.vinicius73@gmail.com>
2023-04-02 16:52:44 -0300

    ➕ add faker

--- src/infra/db/seeds/001--create_users.ts
+++ src/infra/db/seeds/001--create_users.ts
@@ -1,13 +1,16 @@
 import { Knex } from 'knex';
 import { ulid } from 'ulidx';
+import { faker } from '@faker-js/faker/locale/pt_BR';
 
 const createRecord = () => ({
   id: ulid(),
-  name: 'John Doe',
+  name: faker.name.fullName(),
 });
 
 export async function seed(knex: Knex): Promise<void> {
+  const length = faker.datatype.number({ min: 10, max: 100 });
+
   await knex('users').del();
 
-  await knex('users').insert(Array.from({ length: 10 }).map(createRecord));
+  await knex('users').insert(Array.from({ length }).map(createRecord));
 }
